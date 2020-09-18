836ca6a80845a9107d4ce00f477c5b6518ea5cac
Saeran Vasanthakumar <saeranv@gmail.com>
2020-09-18 16:36:50 -0400

    feat(model): Add all tests for final uwg and custom_uwg schema.

--- uwg_schema/simulation.py
+++
@@ -1,9 +0,0 @@
-"""UWG simulation parameter schema."""
-from pydantic import Field, constr
-
-from ._base import NoExtraBaseModel
-
-
-class SimulationParameter(NoExtraBaseModel):
-
-    type: constr(regex='^SimulationParameter$') = 'SimulationParameter'
